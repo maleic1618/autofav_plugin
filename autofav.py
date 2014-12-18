20da22e1ab64196f504644bb4db949a3131f9514
maleic <c4h2o3.maleic.anhydride@gmail.com>
2014-12-18 09:06:25 +0900

    autofav

---
+++ autofav.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python
+#-*- coding:utf-8 -*-
+
+import sys
+import os
+sys.path.append(os.pardir)
+
+from plugin import *
+
+class auto_favorite(Plugin):
+    def __init__(self,mainwindow):
+        super(auto_favorite,self).__init__(mainwindow)
+        self.screen_name=u'bagirom'  "<-userID"
+
+    def on_status(self, status):
+        if status.has_key('event') == False:
+            if status.has_key('text') == True:
+                if status['user']['screen_name'] == self.screen_name:
+                    self.create_favorite(status['id'])
